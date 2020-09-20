66c9dc57970e6cb3a946f2843fcda3f9006ce164
unknown <niels.lueddecke@gmx.net>
2020-09-20 07:23:22 +0200

    rework spi addressing

--- trackerAP/main/rt_spi.h
+++ trackerAP/main/rt_spi.h
@@ -33,34 +33,41 @@ class oo_RT_spi {
 
 // *** addresses for rt over spi
 #define RT_READ_STATE		0x00
-#define RT_WRITE_STATE		0xF0
-#define RT_WRITE_COUNT		0x05
-#define RT_WRITE_FB			0x01
-#define RT_PRINT_CHAR		0x81
-#define RT_SDRAM2REG		0xa0
-#define RT_PEAK2REG			0xa1
-#define RT_REG2SDRAM		0xa2
-#define RT_TUNE_RX0			0x90
-#define RT_TUNE_RX1			0x91
-#define RT_TUNE_RX2			0x92
-#define RT_TUNE_RX3			0x93
-#define RT_TRG_LEVEL0		0x94
-#define RT_TRG_LEVEL1		0x95
-#define RT_TRG_LEVEL2		0x96
-#define RT_TRG_LEVEL3		0x97
-#define RT_PD_DEADTIME		0x98
-#define RT_PD_AUTO_FACTOR	0x99
-#define RT_PD_AUTO_MIN		0x9a
-#define RT_CLR_MINMAX		0x10
-#define RT_MIN_BASE			0x11
-#define RT_MAX_BASE			0x15
-#define RT_NORM_BASE		0x70
-#define RT_QUOT_BASE		0x74
-#define RT_TRIGGER_BASE		0x78
-#define RT_PD_EXC_BASE		0x7c
-#define RT_PD_START			0x02
-#define RT_PD_CLEAR			0x03
-#define RT_PD_EX_CLEAR		0x04
-#define RT_PD_DET_FIXED		0xF1
+#define RT_WRITE_STATE		0x01
+#define RT_WRITE_COUNT		0x02
+
+#define RT_TUNE_RX0			0x10
+#define RT_TUNE_RX1			0x11
+#define RT_TUNE_RX2			0x12
+#define RT_TUNE_RX3			0x13
+
+#define RT_TRG_LEVEL0		0x18
+#define RT_TRG_LEVEL1		0x19
+#define RT_TRG_LEVEL2		0x1a
+#define RT_TRG_LEVEL3		0x1b
+
+#define RT_PD_DEADTIME		0x20
+#define RT_PD_AUTO_FACTOR	0x21
+#define RT_PD_AUTO_MIN		0x22
+#define RT_CLR_MINMAX		0x23
+#define RT_PD_START			0x24
+#define RT_PD_CLEAR			0x25
+#define RT_PD_EX_CLEAR		0x26
+#define RT_PD_DET_FIXED		0x27
+#define RT_PD_EXC_BASE		0x28
+
+#define RT_MIN_BASE			0x30
+#define RT_MAX_BASE			0x38
+#define RT_NORM_BASE		0x40
+#define RT_QUOT_BASE		0x48
+#define RT_TRIGGER_BASE		0x50
+
+#define RT_SDRAM2REG		0x60
+#define RT_PEAK2REG			0x61
+#define RT_REG2SDRAM		0x62
+
+// --- oled
+#define RT_PRINT_CHAR		0x80
+#define RT_WRITE_FB			0x81
 
 #define RT_STATUS_PDREADY	0x01
