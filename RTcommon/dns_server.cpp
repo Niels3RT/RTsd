302509ff4543dda14b2520f4652e8c921a8a598f
unknown <niels.lueddecke@gmx.net>
2020-10-05 21:43:43 +0200

    make dns server play nice

--- RTcommon/dns_server.cpp
+++ RTcommon/dns_server.cpp
@@ -45,15 +45,14 @@ static void dns_server_task(void *pvParameters) {
 			
 			// --- print info to serial log
 			inet_ntoa_r(((struct sockaddr_in *)&source_addr)->sin_addr.s_addr, addr_str, sizeof(addr_str) - 1);
-			ESP_LOGI(TAG, "DNS Server, request from '%s'", addr_str);
+			//ESP_LOGI(TAG, "DNS Server, request from '%s'", addr_str);
 
             // --- Error occurred during receiving
             if (len < 0) {
                 ESP_LOGE(TAG, "DNS Server recvfrom failed: errno %d", errno);
                 break;
-            }
-            // --- request received
-            else {
+			// --- request received
+            } else {
 				// -- print request data
 				//uint8_t lcnt = 0;
 				//char * rx_ptr = &rx_buffer[0];
@@ -68,17 +67,27 @@ static void dns_server_task(void *pvParameters) {
 				//}
 				//printf("\r\n");
 
+				// -- check hostname
+				bool reply_localhost = true;
+				uint8_t hn_len = rx_buffer[12];
+				if ((hn_len > 0) && (hn_len < 63)) {
+					char hn_tmp[64];
+					strncpy(hn_tmp, &rx_buffer[13], hn_len);
+					hn_tmp[hn_len] = '\0';
+					ESP_LOGI(TAG, "DNS Server, request from '%s' for '%s'", addr_str, hn_tmp);
+					if ((hn_len == 4) && (strncmp(hn_tmp, "rtsd", 4) == 0)) reply_localhost = false;
+				} else {
+					ESP_LOGI(TAG, "DNS Server, malformed request from '%s'", addr_str);
+					break;
+				}
+				 
 				// -- parse request, modify into reply
-				// - will be reply, set bit 1
-				rx_buffer[2] |= 0x80;
-				// - add 1 answer record
-				rx_buffer[6] = 0x00;
-				rx_buffer[7] = 0x01;
+				// - will be reply, set msb to 1, is authoritative answer bit 0x04
+				rx_buffer[2] |= 0x84;
 				// - walk request labels
 				uint8_t pos = 12;	// set pointer to length of first label
 				while(1) {
 					if (rx_buffer[pos] != 0) {
-						//printf ("pos '%d' length'%d'\r\n", pos, rx_buffer[pos]);
 						pos += rx_buffer[pos] + 1;
 					} else {
 						pos++;
@@ -86,33 +95,56 @@ static void dns_server_task(void *pvParameters) {
 					}
 				}
 				// - type
+				int r_type = (rx_buffer[pos] << 8) + rx_buffer[pos+1];
 				pos += 2;
 				// - class
+				int r_class = (rx_buffer[pos] << 8) + rx_buffer[pos+1];
 				pos += 2;
 				
-				// -- add resource record for reply
-				// - point to requested host name
-				rx_buffer[pos++] = 0xc0;
-				rx_buffer[pos++] = 0x0c;
-				// - is type A, host address
-				rx_buffer[pos++] = 0x00;
-				rx_buffer[pos++] = 0x01;
-				// - class IN
-				rx_buffer[pos++] = 0x00;
-				rx_buffer[pos++] = 0x01;
-				// - time to live, 600s, 10 minutes
-				rx_buffer[pos++] = 0x00;
-				rx_buffer[pos++] = 0x00;
-				rx_buffer[pos++] = 0x02;
-				rx_buffer[pos++] = 0x58;
-				// - data length, 4 octets of ip
-				rx_buffer[pos++] = 0x00;
-				rx_buffer[pos++] = 0x04;
-				// - host ip address
-				rx_buffer[pos++] = dnss.host_ap_addr.ip8x4[0];
-				rx_buffer[pos++] = dnss.host_ap_addr.ip8x4[1];
-				rx_buffer[pos++] = dnss.host_ap_addr.ip8x4[2];
-				rx_buffer[pos++] = dnss.host_ap_addr.ip8x4[3];
+				// -- is request type A (host address)?
+				if ((r_type == 0x0001) && (r_class == 0x0001)) {
+					// - add 1 answer record
+					rx_buffer[6] = 0x00;
+					rx_buffer[7] = 0x01;
+					// -- add resource record for reply
+					// - point to requested host name
+					rx_buffer[pos++] = 0xc0;
+					rx_buffer[pos++] = 0x0c;
+					// - is type A, host address
+					rx_buffer[pos++] = 0x00;
+					rx_buffer[pos++] = 0x01;
+					// - class IN
+					rx_buffer[pos++] = 0x00;
+					rx_buffer[pos++] = 0x01;
+					// - time to live, 600s, 10 minutes
+					rx_buffer[pos++] = 0x00;
+					rx_buffer[pos++] = 0x00;
+					rx_buffer[pos++] = 0x02;
+					rx_buffer[pos++] = 0x58;
+					// - data length, 4 octets of ip
+					rx_buffer[pos++] = 0x00;
+					rx_buffer[pos++] = 0x04;
+					// - host ip address
+					if (!reply_localhost) {
+						// reply host ip
+						rx_buffer[pos++] = dnss.host_ap_addr.ip8x4[0];
+						rx_buffer[pos++] = dnss.host_ap_addr.ip8x4[1];
+						rx_buffer[pos++] = dnss.host_ap_addr.ip8x4[2];
+						rx_buffer[pos++] = dnss.host_ap_addr.ip8x4[3];
+					} else {
+						// reply localhost to divert requests not meant for us
+						rx_buffer[pos++] = 127;
+						rx_buffer[pos++] = 0;
+						rx_buffer[pos++] = 0;
+						rx_buffer[pos++] = 1;
+					}
+				} else {
+					// - reply 'not implemented', at least try to play nice :P
+					rx_buffer[3] = 0x04;
+					// - don't write answer record
+					rx_buffer[6] = 0x00;
+					rx_buffer[7] = 0x00;
+				}
 				
 				// -- send reply
                 int err = sendto(sock, rx_buffer, pos, 0, (struct sockaddr *)&source_addr, sizeof(source_addr));
@@ -120,10 +152,9 @@ static void dns_server_task(void *pvParameters) {
                     ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
                     break;
                 }
-				
-				// -- clear buffer
-				memset(rx_buffer, 0, sizeof(rx_buffer));
             }
+			// -- clear buffer
+			memset(rx_buffer, 0, sizeof(rx_buffer));
         }
 
         if (sock != -1) {
